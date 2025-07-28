825876810a4b87b0a4271850f9b457f9bc999386
tic-fhce <tic.fhce@gmail.com>
2025-07-27 22:36:46 -0400

    cambio de seleccion y modificacion de observaciones

--- src/main/java/com/fhce/scc/dao/obsBioDao.java
+++ src/main/java/com/fhce/scc/dao/obsBioDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.fhce.scc.model.obsBioModel;
 
 public interface obsBioDao extends JpaRepository<obsBioModel,Long>{
-	@Query(value = "select * from obsbio where _02idobs=? and _09estado<2 order by id desc",nativeQuery=true)
+	@Query(value = "select * from obsbio where _02idobs=? and _09estado<3 order by id desc",nativeQuery=true)
 	List<obsBioModel>getIdObs(Long idObs);
 	
 	@Query(value = "select * from obsbio where _02idobs=? and _09estado=? order by id desc",nativeQuery=true)
